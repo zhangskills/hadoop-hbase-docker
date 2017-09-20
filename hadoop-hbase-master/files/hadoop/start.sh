0eb361b5f4647c9bc75512023d83aea7aebb9b87
张孟杰 <zhangskills@gmail.com>
2017-09-20 17:24:59 +0800

    start-ssh-serf.sh 改为 start.sh

--- hadoop-hbase-master/files/hadoop/start.sh
+++ hadoop-hbase-master/files/hadoop/start.sh
@@ -4,12 +4,4 @@
 echo "start sshd..."
 service ssh start
 
-# start sef
-echo -e "\nstart serf..." 
-/etc/serf/start-serf-agent.sh > serf_log &
-
-sleep 5
-
-serf members
-
 echo -e "\nhadoop-hbase-cluster-docker "
---
+++ hadoop-master/files/init/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# start sshd
+echo "start sshd..."
+service ssh start
+
+echo -e "\nhadoop-cluster-docker "
